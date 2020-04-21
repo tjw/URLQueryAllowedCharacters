1b88eaba945cb9090277d876eb084a4d9e52c699
Timothy J. Wood <tjw@omnigroup.com>
2020-04-21 14:16:30 -0700

    Initial Commit

---
+++ URLQueryAllowedCharacters/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  URLQueryAllowedCharacters
+//
+//  Created by Timothy Wood on 4/21/20.
+//  Copyright © 2020 The Omni Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
